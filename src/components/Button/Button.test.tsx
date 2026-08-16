0dffd86128906a75e71efe79a7b0f7966fd3fb83
Filipe <filipesilveira.dev@gmail.com>
2026-08-16 19:23:36 -0300

    test: teste de dispsrar função no onClick do Button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,15 +1,38 @@
-import { describe, expect, it } from "@jest/globals";
+import { describe, expect, it, jest } from "@jest/globals";
 import { render, screen } from "@testing-library/react";
+// importa o botão a ser testado
 import Button from "./Button";
 
 describe("Button", () => {
   it("deve renderizar o conteúdo recebido", () => {
+    // função do testing library que renderiza compontentes react
     render(<Button>Rolar dado</Button>);
 
+    // variável que recebe um elemento em tela com o texto "Rolar dado"
     const button = screen.getByRole("button", {
       name: "Rolar dado",
     });
 
+    // expectativa que "button" seja verdadeiro
     expect(button).toBeTruthy();
+  });
+
+  it("deve executar a função ao ser clicado", () => {
+    // funlção mock criada para verificar se foi chamada
+    const handleClick = jest.fn();
+
+    // renderiza o botão com a mock function no  onClick
+    render(<Button onClick={handleClick}>Rolar dado</Button>);
+
+    // variável recebe o botão com o texto "Rolar dado"
+    const button = screen.getByRole("button", {
+      name: "Rolar dado",
+    });
+
+    // simulação ação de "click" no button
+    button.click();
+
+    // expectativa de que a mock function tenha sido chamada uma vez
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
